3fab1a7febd40defc7d6d6854c782611ae338163
hubekraven <hubefox@gmail.com>
2017-06-23 22:32:34 -0400

    correction du ScriptEchelle, ajout du passage de niveau

--- Assets/scripts/salle/ScriptEchelle.cs
+++ Assets/scripts/salle/ScriptEchelle.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class ScriptEchelle : MonoBehaviour {
 
+	private Scene currentLevel;//niveau actuel
+	private int iLevel;//index du niveau actuel
+
 	void OnTriggerEnter2D(Collider2D Other){
-		if (Other.transform.tag == "Player") {
-			SceneManager.LoadScene ("Fin");
+
+		if (Other.transform.tag == "Player") 
+		{
+			currentLevel = SceneManager.GetActiveScene ();
+
+			if (currentLevel.name != "Niveau4") 
+			{
+				iLevel = currentLevel.buildIndex;
+				SceneManager.LoadScene (iLevel+1);
+			}
+			else
+			{
+				SceneManager.LoadScene ("Fin");
+			}
 		}
 	}
 
